f42dcf4c3f6622b8946f313e80df85c11dc16b7b
Pa Gutri <pagutri@gmail.com>
2022-04-19 15:24:32 -0500

    Agregar ejemplo 4 de objetos teitter

---
+++ weekly_mission_2/exercises/ejercicio1/04twitter.js
@@ -0,0 +1,13 @@
+const user = {
+    user: "carlo",
+    username: "carlogilmar",
+    bio: "Code at @ErlangSolutions",
+    location: "Mexico City",
+    followers: 2502,
+    following: 1183
+}
+
+const trending_topic = {
+    hashtags: ["lorde", "swiftie"]
+}
+
