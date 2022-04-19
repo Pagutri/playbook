b1c461ea06aca58adf9007fe06a458608e00be0f
Pa Gutri <pagutri@gmail.com>
2022-04-19 14:05:37 -0500

    Crear issue del ejercicio 1

---
+++ weekly_mission_2/exercises/ejercicio1/02issue.js
@@ -0,0 +1,21 @@
+const issue = {
+    title: "Package number two not working",
+    repositoryNameAssociated: "MyPackages",
+    status: "open",
+    numberOfComments: 100,
+    labels: ["urgent", "javascript", "version 1"],
+    author: "Pagutri",
+    dateCreated: "04-18-2022",
+    lastUpdated: "04-19-2022",
+    getTitleAndAuthor: function(){
+      return [this.title, this.author]
+    },
+    getGeneralInfo: function(){
+      return `This issue ${this.title} was created by ${this.author}`
+    }
+   }
+   
+   console.log("Título del issue:" + issue.getTitleAndAuthor()[0])
+   console.log("Autor del issue: " + issue.getTitleAndAuthor()[1])
+   console.log(issue.getGeneralInfo())
+   
