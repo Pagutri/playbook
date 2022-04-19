c407f06ccd1485b31481a4fbab45f94391a19228
Pa Gutri <pagutri@gmail.com>
2022-04-19 11:04:39 -0500

    Corregir ejemplo 6 operadores

--- weekly_mission_2/ejemplos_operadores/06map.js
+++ weekly_mission_2/ejemplos_operadores/06map.js
@@ -1,4 +1,4 @@
-// Ejemplo 6: Uso de map para convertir todos los nombres de una lista a mayúsculas
+// Ejemplo 6: Uso de map para convertir las primeras tres letras de todos los nombres de una lista a mayúsculas
 const countries6 = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
 const countriesFirstThreeLetters = countries6.map((country) =>
   country.toUpperCase().slice(0, 3) // el método slice obtiene solo la longitud marcada del string
